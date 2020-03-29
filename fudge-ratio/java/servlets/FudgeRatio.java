09387968761c58286c04f23a86dbf8c8e64b3aef
Yaa Owusu <yaaowusu00@gmail.com>
2020-03-28 23:32:11 -0400

    fix Oscar's suggestions in servlet

--- fudge-ratio/java/servlets/FudgeRatio.java
+++ fudge-ratio/java/servlets/FudgeRatio.java
@@ -1,11 +1,6 @@
 package com.google.sps.servlets;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
-import com.google.cloud.translate.Translation;
-import com.google.gson.Gson;
-import java.util.*; 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
@@ -25,25 +20,31 @@ public class DataServlet extends HttpServlet {
     }
 
 
-
+    /**
+     * Changes the fudege ratio (actual/expected time) by converting start and end time to seconds.
+     * @param start   This is the user's start time for a task. 
+     *                This is a string in the format "00:00:00" (hrs:min:second)
+     *
+     * @param end     This is the end time of the task, in the 
+     *                same format as the start string.
+     */
     private double calculateRatio(String start, String end, int expected){
-        //parse the starting time time string (in format 00:00:00 (hrs:min:second))
-        int startHr = Integer.parseInt(start.substring(0, 2));
-        int startMin = Integer.parseInt(start.substring(3, 5));
-        int startSec = Integer.parseInt(start.substring(6, 8));
+        //parse the starting time string 
+        final int startHr = Integer.parseInt(start.substring(0, 2));
+        final int startMin = Integer.parseInt(start.substring(3, 5));
+        final int startSec = Integer.parseInt(start.substring(6, 8));
 
-        //parse the ending time time string (in format 00:00:00 (hrs:min:second))
-        int endHr = Integer.parseInt(end.substring(0, 2));
-        int endMin = Integer.parseInt(end.substring(3, 5));
-        int endSec = Integer.parseInt(end.substring(6, 8));
+        //parse the ending time string 
+        final int endHr = Integer.parseInt(end.substring(0, 2));
+        final int endMin = Integer.parseInt(end.substring(3, 5));
+        final int endSec = Integer.parseInt(end.substring(6, 8));
 
-        //convert the starting time to minutes
-        double startInMinutes = (startHr * 60) + startMin + (startSec / 60);
-        //convert the ending time to minutes
-        double endInMinutes = (endHr * 60) + endMin + (endSec / 60);
+        //convert the starting time to seconds
+        double startInSeconds = (startHr * 3600) + (startMin * 60) + startSec;
+        //convert the ending time to seconds
+        double endInSeconds = (endHr * 3600) + (endMin * 60) + endSec;
         //calculate duration and ratio
-        double actualDuration = endInMinutes - startInMinutes;
+        double actualDuration = endInSeconds  - startInSeconds;
         double ratio = actualDuration / expected; 
         return ratio;
     }
-}
