0148204c2490dae419f8f2f3cfbcfec5cd774f92
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-29 02:05:36 -0600

    Add clear button

--- fudge-ratio/src/main/webapp/script.js
+++ fudge-ratio/src/main/webapp/script.js
@@ -1,4 +1,5 @@
 const button = document.getElementById("startStopButton");
+const buttonClear = document.getElementById("clear");
 const text = document.getElementById("timer");
 button.textContent = "Start";
 
@@ -10,16 +11,6 @@ let timer = null;
 // Concatenates a zero to the beginning of a number less than 10.
 const padd = num => (num < 10 ? "0" : "") + num;
 
-// Checks when the start and stop buttons are clicked once the HTML page is loaded.
-window.addEventListener("load", activateListeners);
-
-// Waits for the user to click start or stop to run their respective functions.
-function activateListeners() {
-    document.getElementById("start-button").addEventListener("click", startTimer);
-    document.getElementById("stop-button").addEventListener("click", stopTimer);
-}
-
-// Updates the timer text in index.html to increment every second, minute, and hour.
 const addOneSecond = () => {
   seconds++;
   if (seconds == 60) {
@@ -32,13 +23,17 @@ const addOneSecond = () => {
   }
 };
 
+// Updates the timer text in index.html to increment every second, minute, and hour.
+const updateText = () =>
+  (text.textContent = padd(hours) + ":" + padd(minutes) + ":" + padd(seconds));
+
 // When the start button is clicked, the timer variable is initialized to 1 every second.
 const startTimer = () => {
   if (timer) return;
   timer = setInterval(() => {
     addOneSecond();
-    text.textContent = padd(hours) + ":" + padd(minutes) + ":" + padd(seconds);
-  }, 1000);
+    updateText();
+  }, 100);
 };
 
 // Stops the increment of hours, minutes, and seconds on the timer.
@@ -47,14 +42,20 @@ const stopTimer = () => {
   timer = null;
 };
 
-button.addEventListener("click", event => {
+button.addEventListener("click", () => {
   if (button.textContent === "Start") {
     const minutesSince = startTimer();
   } else {
     stopTimer();
   }
 
-  button.classList.toggle("stop")
-  text.classList.toggle("stopped")
+  button.classList.toggle("stop");
+  text.classList.toggle("stopped");
   button.textContent = button.textContent === "Start" ? "Stop" : "Start";
+});
+
+buttonClear.addEventListener("click", () => {
+  if (button.textContent === "Stop") button.click();
+  (seconds = 0), (minutes = 0), (hours = 0);
+  updateText();
 });
