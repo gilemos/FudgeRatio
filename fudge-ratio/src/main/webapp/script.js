df92a0478bed7fe2aa7cd0f5baf5d8fffae435ba
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-31 16:21:39 -0600

    Run prettier

--- fudge-ratio/src/main/webapp/script.js
+++ fudge-ratio/src/main/webapp/script.js
@@ -8,7 +8,7 @@ let seconds = 0,
   hours = 0;
 let timer = null;
 let task = "";
-let duration = " PLACE HOLDER" //leaving this here for now because it might interfere with how Jerry wants to do things
+let duration = " PLACE HOLDER"; //leaving this here for now because it might interfere with how Jerry wants to do things
 
 // Concatenates a zero to the beginning of a number less than 10.
 const padd = num => (num < 10 ? "0" : "") + num;
@@ -42,19 +42,18 @@ const startTimer = () => {
 const stopTimer = () => {
   clearInterval(timer);
   timer = null;
-   displayMessage();
+  displayMessage();
 };
 
 button.addEventListener("click", () => {
   if (button.textContent === "Start") {
-    if (document.getElementById('dropdown-contents').value == "") {
-        alert("You must pick a task before starting");
-        return;
-    } 
-    task = document.getElementById('dropdown-contents').value;
+    if (document.getElementById("dropdown-contents").value == "") {
+      alert("You must pick a task before starting");
+      return;
+    }
+    task = document.getElementById("dropdown-contents").value;
     const minutesSince = startTimer();
-  } 
-  else {
+  } else {
     stopTimer();
   }
   button.classList.toggle("stop");
@@ -69,19 +68,18 @@ buttonClear.addEventListener("click", () => {
 });
 
 const formatTaskString = () => {
-    let dropdown = document.getElementById("dropdown-contents");
-    let op = dropdown.options[dropdown.selectedIndex];
-	let category = op.parentNode.label;
-    let formattedString = "";
-    if (category == "Schoolwork"){
-        formattedString = task + " homework!";
-    }
-    else if (category == "Job-related tasks"){
-        formattedString = "doing " + task + "!" ;
-    }
-    else formattedString = task + "!"; 
-    return formattedString; 
-}
+  let dropdown = document.getElementById("dropdown-contents");
+  let op = dropdown.options[dropdown.selectedIndex];
+  let category = op.parentNode.label;
+  let formattedString = "";
+  if (category == "Schoolwork") {
+    formattedString = task + " homework!";
+  } else if (category == "Job-related tasks") {
+    formattedString = "doing " + task + "!";
+  } else formattedString = task + "!";
+  return formattedString;
+};
 const displayMessage = () => {
-    document.getElementById("finalMessage").innerHTML = "Next time please allocate" + duration + " for "  + formatTaskString();
+  document.getElementById("finalMessage").innerHTML =
+    "Next time please allocate" + duration + " for " + formatTaskString();
 };
