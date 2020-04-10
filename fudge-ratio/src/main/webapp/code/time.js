10b545305197a1856018be2b12e3a23418b8630c
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-10 15:42:53 -0500

    Small implementation details in timer.js

--- fudge-ratio/src/main/webapp/code/time.js
+++ fudge-ratio/src/main/webapp/code/time.js
@@ -1,46 +1,40 @@
 class Timer {
   seconds = 0;
-  interval = null;
+  constructor(time = "00:00:00") {
+    const seconds = parseInt(time.substring(6, 8));
+    const minutes = 60 * parseInt(time.substring(3, 5));
+    const hours = 3600 * parseInt(time.substring(0, 2));
 
-  constructor(time) {
-    if (time) {
-      const seconds = parseInt(time.substring(6, 8));
-      const minutes = 60 * parseInt(time.substring(3, 5));
-      const hours = 3600 * parseInt(time.substring(0, 2));
-
-      this.seconds = seconds + minutes + hours;
-    }
+    this.seconds = seconds + minutes + hours;
   }
 
   // Concatenates a zero to the beginning of a number less than 10.
-  padd(num) {
+  padd(num = 0) {
     const padding = num < 10 ? "0" : "";
-    return padding + num;
+    return padding + num.toString();
   }
 
-  addSeconds(howMany) {
+  addSeconds(howMany = 0) {
     this.seconds = Math.max(0, howMany + this.seconds);
     return this;
   }
 
-  getHuman() {
-    const seconds = this.seconds % 60;
-    const minutes = Math.floor(this.seconds / 60) % 60;
-    const hours = Math.floor(this.seconds / 3600) % 24;
-    return [seconds, minutes, hours];
-  }
-
   getText() {
-    const [seconds, minutes, hours] = this.getHuman();
-    return this.padd(hours) + ":" + this.padd(minutes) + ":" + this.padd(seconds);
+    const seconds = this.padd(this.seconds % 60);
+    const minutes = this.padd(Math.floor(this.seconds / 60) % 60);
+    const hours = this.padd(Math.floor(this.seconds / 3600) % 24);
+
+    return `${hours}:${minutes}:${seconds}`;
   }
 
-  updateTimer(elapsed) {
+  updateTimer(elapsed = 0) {
     this.seconds = elapsed;
+    return this;
   }
 
   clear() {
     this.seconds = 0;
+    return this;
   }
 }
 
--- fudge-ratio/src/main/webapp/code/utils.js
+++ fudge-ratio/src/main/webapp/code/utils.js
@@ -1,5 +1,4 @@
 const get = id => document.getElementById(id);
-
 const createFromText = text => document.createRange().createContextualFragment(text);
 const parent = document.getElementById("content");
 
