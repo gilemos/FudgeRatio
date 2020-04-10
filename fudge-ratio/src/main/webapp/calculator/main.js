4716adc15cd6c06082645ca5ba69a8c148b3b50b
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-10 03:43:01 -0500

    Now each file (aka component) is really a lot more modular

--- fudge-ratio/src/main/webapp/calculator/main.js
+++ fudge-ratio/src/main/webapp/calculator/main.js
@@ -1,83 +1,16 @@
-import { saveSessionState, recoverSessionState } from "../code/save.js";
-import { cleanSessionState, isNotEmpty } from "../code/save.js";
-import loadActivities from "../code/loadActivities.js";
-import displayMessage from "../code/nextTimeAllocate.js";
-import Timer from "../code/time.js";
-import "../code/expectedTime.js";
+import showExpectedTime from "../code/expectedTime.js";
+import showActivities from "../code/chooseActivities.js";
+import showMain from "../code/mainScreen.js";
 
-const button = document.getElementById("startStopButton");
-const buttonClear = document.getElementById("clear");
-const text = document.getElementById("timer");
-const dropdown = document.getElementById("dropdown-contents");
+// Cool intro
+document.body.style.opacity = "1";
 
-button.textContent = "Start";
-
-const timer = new Timer();
-
-button.addEventListener("click", () => {
-  if (button.textContent === "Start") {
-    if (dropdown.value == "") {
-      alert("You must pick a task before starting");
-      return;
-    }
-    const minutesSince = startTimer();
-    saveSessionState(dropdown);
-  } else {
-    stopTimer();
-  }
-  button.classList.toggle("stop");
-  text.classList.toggle("stopped");
-  button.textContent = button.textContent === "Start" ? "Stop" : "Start";
-});
-
-// Recover state
-
-loadActivities(() => {
-  if (isNotEmpty()) {
-    const [timeStarted, selectedIndex] = recoverSessionState();
-    const start = parseInt(timeStarted);
-
-    setTimeout(() => {
-      dropdown.selectedIndex = selectedIndex;
-      const now = Date.now();
-      const elapsed = Math.floor((now - start) / 1000);
-
-      timer.updateTimer(elapsed);
-      text.textContent = timer.getText();
-      button.click();
-    }, 1000);
-  }
-});
-
-// When the start button is clicked, the timer variable is initialized to 1 every second.
-const startTimer = () => {
-  if (timer.interval) return;
-  timer.interval = setInterval(() => {
-    timer.addOneSecond();
-    text.textContent = timer.getText();
-  }, 1000);
+const App = async () => {
+  const expectedTime = await showExpectedTime();
+  console.log(expectedTime);
+  const activity = await showActivities();
+  console.log(activity);
+  const ratio = await showMain();
 };
 
-// Stops the increment of hours, minutes, and seconds on the timer.
-const stopTimer = () => {
-  clearInterval(timer.interval);
-  timer.interval = null;
-  cleanSessionState();
-
-  displayMessage(dropdown, timer.getText(), localStorage.getItem("expected"));
-  localStorage.removeItem("expected");
-};
-
-buttonClear.addEventListener("click", () => {
-  if (button.textContent === "Stop") button.click();
-  timer.clear();
-  text.textContent = timer.getText();
-  cleanSessionState();
-  localStorage.clear();
-  dropdown.selectedIndex = 0;
-});
-
-dropdown.addEventListener("change", () => {
-  const current = recoverSessionState()[1];
-  if (current) dropdown.selectedIndex = current;
-});
+App();
