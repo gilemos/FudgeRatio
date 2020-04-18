72a779528bebabc912c83dbeebd86a07eb5ffd8c
Yaa Owusu <yaaowusu00@gmail.com>
2020-04-18 19:44:03 -0400

    add mobile about page, cleans up some style in about and landing

--- fudge-ratio/src/main/webapp/about/about.js
+++ fudge-ratio/src/main/webapp/about/about.js
@@ -1,8 +1,30 @@
-function openTab(tabName) {
-  let i, x;
-  x = document.getElementsByClassName("bioContainer");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
+let currentSlide = 0;
+
+const openTab = tabName => {
+  Array.from(document.getElementsByClassName("bioContainer"))
+    .forEach(bio => bio.style.display = bio.id === tabName? "block": "none");
+}
+const closeTab = () => {
+  Array.from(document.getElementsByClassName("bioContainer"))
+    .forEach(bio => bio.style.display = "none");
+}
+
+const slideDivs = direction => {
+    let newSlide = currentSlide + direction;
+    currentSlide =  newSlide > 3? 0
+                 :  newSlide < 0? 3
+                 :  newSlide; 
+    let slides = Array.from(document.getElementsByClassName("slide"));
+    slides.forEach((slide, index) => {
+        let children = Array.from(slide.children);
+        children.forEach((child) => {
+            slide.style.display = currentSlide === index? "block": "none"
+            child.style.display = currentSlide === index? "block": "none"
+        })
+    });
+}
+
+const expandNav = () => { 
+    let links = document.getElementById("links");
+    links.style.display = links.style.display === "block"? "none": "block";
 }
