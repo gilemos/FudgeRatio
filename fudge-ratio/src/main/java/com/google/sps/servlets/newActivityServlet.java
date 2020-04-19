5a6816a8fa950d8129468a5cb442faccc6654721
Giovanna Lemos <gigi18377@gmail.com>
2020-04-19 16:08:33 -0400

    Creates code to set up datastore

--- fudge-ratio/src/main/java/com/google/sps/servlets/newActivityServlet.java
+++ fudge-ratio/src/main/java/com/google/sps/servlets/newActivityServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/new-act")
 public class newActivityServlet extends HttpServlet {
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
