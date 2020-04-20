2640f8599f6ab4e3bfbc417cbe26554e4e52fec7
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-20 01:52:17 -0500

    Small fixes

--- fudge-ratio/src/main/webapp/code/save.js
+++ fudge-ratio/src/main/webapp/code/save.js
@@ -1,6 +1,7 @@
 const TIME_KEY = "startingTime";
+const FINAL_KEY = "finalDuration";
 const ACTIVITY_KEY = "selected";
 const EXPECTED_KEY = "expected";
 
-const KEYS = { TIME_KEY, ACTIVITY_KEY, EXPECTED_KEY };
+const KEYS = { TIME_KEY, ACTIVITY_KEY, EXPECTED_KEY, FINAL_KEY };
 export { KEYS };
